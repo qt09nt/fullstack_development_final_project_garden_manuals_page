1566de5370e009ced080d59dea08ab693a3bb989
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2024-06-06 18:38:49 -0600

    Add files via upload

---
+++ backend/connection.js
@@ -0,0 +1,15 @@
+//const Sequelize = require('sequelize');
+const mariadb = require('mariadb');
+require('dotenv').config();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    connectTimeout: 30000,
+});
+
+module.exports = pool;
+
