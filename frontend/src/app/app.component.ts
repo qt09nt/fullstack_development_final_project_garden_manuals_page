8b63861413927ad296849f5ea64eba8dc1bad415
Queenie <qt09nt@gmail.com>
2024-06-09 13:43:21 -0600

    commented out <app-root> in app.component.html and now the plant categories shows up again

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,14 +13,7 @@ import { PlantsComponent } from './components/plants/plants.component';
   // ],
   styleUrl: './app.component.css',
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
+  <h1>Hello world!</h1>
 `,
 })
 
