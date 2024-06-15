23f47645bea8edeb73d23bcd3871386a1cc2287b
Queenie <qt09nt@gmail.com>
2024-06-14 23:53:48 -0600

    sign up forms validation

--- frontend/src/app/sign-up-validation/sign-up-validation.component.ts
+++ frontend/src/app/sign-up-validation/sign-up-validation.component.ts
@@ -13,3 +13,16 @@ export class SignUpValidationComponent {
     password:''
   }
 }
+
+// getter methods for form controls
+// get nameFormControl(){
+//   return this.studentForm.get('name')!; //return the name form control
+// }
+
+// get emailFormControl(){
+//   return this.studentForm.get('email')!; //return the email form control
+// }
+
+// get phoneFormControl(){
+//   return this.studentForm.get('phone_number')!; //return the name form control
+// }
